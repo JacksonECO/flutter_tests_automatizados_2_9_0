07aac9f9b31ec4515880d54b5d642cc71d170731
Jackson Galdino <jacksongdas.jackson@gmail.com>
2021-12-29 21:35:17 -0300

    Aula 4.5

--- test/flows/transfer_flow_test.dart
+++ test/flows/transfer_flow_test.dart
@@ -1,6 +1,8 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -11,6 +13,8 @@ import '../helps/mocks.dart';
 main() {
   testWidgets('Should transfer to contact', (tester) async {
     final MockContactDao mockContactDao = MockContactDao();
+    when(mockContactDao.findAll()).thenAnswer((realInvocation) async => [Contact(0, 'Alex', 1000)]);
+
     await tester.pumpWidget(BytebankApp(contactDao: mockContactDao));
 
     final dashboard = find.byType(Dashboard);
@@ -28,5 +32,19 @@ main() {
     // Verifica de a função foi chamada pelo menos uma vez com o verify do mockito,
     // Também é possível definir a quantidade que foi chamada da seguinte forma forma
     verify(mockContactDao.findAll()).called(1);
+
+    final contactItem = find.byWidgetPredicate((widget) {
+      if (widget is ContactItem) {
+        return widget.contact.name == 'Alex' && widget.contact.accountNumber == 1000;
+      }
+      return false;
+    });
+    expect(contactItem, findsOneWidget);
+
+    await tester.tap(contactItem);
+    await tester.pumpAndSettle();
+
+    final transactionForm = find.byType(TransactionForm);
+    expect(transactionForm, findsOneWidget);
   });
 }
